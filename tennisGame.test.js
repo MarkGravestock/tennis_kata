8eb3d52306aa712fe6e2263631b11b2b95167d20
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-07-16 14:46:02 +0100

    Convert to using private field.

--- tennisGame.test.js
+++ tennisGame.test.js
@@ -1,19 +1,19 @@
 test('when a new game is started then the score is 0:0', () => {
-    expect(new Game().score).toBe("0:0");
+    expect(new Game().score()).toBe("0:0");
 });
 
 
 test('when server wins the first point in a game then score is 15:0', () => {
     const game = new Game();
     game.serverWins();
-    expect(game.score).toBe("15:0");
+    expect(game.score()).toBe("15:0");
 });
 
 test('given the score is 15:0 when receiver wins the next point then the score is 15:15', () => {
     const game = new Game();
     game.serverWins();
     game.receiverWins();
-    expect(game.score).toBe("15:15");
+    expect(game.score()).toBe("15:15");
 });
 
 test('given the score is 15:15 when receiver wins the next point then the score is 15:30', () => {
@@ -21,23 +21,32 @@ test('given the score is 15:15 when receiver wins the next point then the score 
     game.serverWins();
     game.receiverWins();
     game.receiverWins();
-    expect(game.score).toBe("15:30");
+    expect(game.score()).toBe("15:30");
 });
+
+/*test('i can\'t see internalScore', () => {
+    const game = new Game();
+    expect(game.#score).toBe("0:0");
+});*/
 
 class Game
 {
-    score = "0:0";
+    #score = "0:0";
+
+    score() {
+        return this.#score;
+    }
 
     serverWins() {
-        this.score = "15:0"
+        this.#score = "15:0"
     }
 
     receiverWins() {
-        if (this.score === "15:15") {
-            this.score = "15:30";
+        if (this.#score === "15:15") {
+            this.#score = "15:30";
             return;
         }
 
-        this.score = "15:15"
+        this.#score = "15:15"
     }
 }
